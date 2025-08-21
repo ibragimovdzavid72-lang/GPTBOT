09ab606c8db0db0d3ad04f16059cea9f0f5f134a
ibragimovdzavid72-lang <ibragimovdzavid72@gmail.com>
2025-08-21 16:52:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,1 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
-
-application = Application.builder().token("
-8450495463:AAHgqcdBVJdgT0vfw6vbXTSu5flmYYiqpz8").build()
-
-async def start(update, context):
-    await update.message.reply_text("Привет, бот работает!")
-
-application.add_handler(CommandHandler("start", start))
 
